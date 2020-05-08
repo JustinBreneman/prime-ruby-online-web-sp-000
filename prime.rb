ad37a309de3ff389d3e5b4706264aa3336e4611e
Justin Breneman <justin.breneman@yahoo.com>
2020-05-08 23:18:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
-num_array = []
-num_array_2 = []
 
 def prime?(num)
   num = num.abs
+  num_array = []
+  num_array_2 = []
   if num <= 1
     false
   elsif num == 2
@@ -16,11 +16,9 @@ def prime?(num)
     end
     num_array.shift
     num_array.pop
-    new_num = (num_array.detect { |x| num % x != 0 })
-    if new_num == nil
-      true
-    else
-      false
+    num_array.each do |x|
+      num_array_2 << num / x
+      binding.pry
     end
   end
 end
