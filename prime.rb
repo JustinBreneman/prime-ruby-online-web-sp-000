8ab627a729b1c32b0d5ce3481cbb8501c39e39cf
Justin Breneman <justin.breneman@yahoo.com>
2020-05-08 23:01:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+require 'pry'
+
+
+def prime?(num)
+  num = num.abs
+  original = num
+  num_array = []
+  num_array_2 = []
+  if num <= 1
+    false
+  elsif num == 2
+    true
+  else
+    num.times do
+      num_array << num
+      num -= 1
+    end
+    num_array.shift
+    num_array.pop
+    new_num = (num_array.detect { |x| num % x != 0 })
+    if new_num == nil
+      true
+    else
+      false
+    end
+  end
+end
