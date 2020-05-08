471a281b0afafd845df7de98dd00aa64ec6ab236
Justin Breneman <justin.breneman@yahoo.com>
2020-05-08 23:40:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -16,7 +16,7 @@ def prime?(num)
     end
     num_array.shift
     num_array.pop
-    num_array.each { |x| num_array_2 << (num % x).to_f }
+    num_array.each { |x| num_array_2 << (num / x).to_f }
     if num_array_2.detect { |y| y > 0.0 }
       false
     else
